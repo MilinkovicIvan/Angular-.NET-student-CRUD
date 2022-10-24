a42c5e41a967e27129225726b5778c5bae62eb4d
MilinkovicIvan <ivan.milinkovic91@gmail.com>
2022-10-24 09:51:59 +0100

    initial .net web api setup created, connection with SQL and creation of database done, api contain working getAllStudents and addStudent request

---
+++ API/studentCRUD.API/studentCRUD.API/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using studentCRUD.API.Data;
+using studentCRUD.API.Models;
+
+namespace studentCRUD.API.Controllers
+{
+    //declare api controller to get rid of View
+    [ApiController]
+    //route
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        private readonly StudentCRUDDBContext _studentCRUDDBContext;
+
+        public StudentsController(StudentCRUDDBContext studentCRUDDBContext)
+        {
+            _studentCRUDDBContext = studentCRUDDBContext;
+        }
+
+
+        //methods
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _studentCRUDDBContext.Students.ToListAsync();
+
+            return Ok(students);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent([FromBody]Student studentRequest)
+        {
+            studentRequest.Id = Guid.NewGuid();
+
+            await _studentCRUDDBContext.Students.AddAsync(studentRequest);
+            await _studentCRUDDBContext.SaveChangesAsync();
+
+            return Ok(studentRequest);
+        }
+    }
+}
---
+++ API/studentCRUD.API/studentCRUD.API/data/StudentCRUDDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentCRUD.API.Models;
+
+namespace studentCRUD.API.Data
+{
+    public class StudentCRUDDBContext : DbContext
+    {
+        //options constructor, when passed options it will also pass them to base class
+        public StudentCRUDDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //entity framework will use this DBSet property to create table
+        public DbSet<Student> Students { get; set; }
+    }
+}
